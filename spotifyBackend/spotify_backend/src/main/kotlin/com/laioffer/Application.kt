47e4646a321ab0bc1e28ddbeebb83df07d64f0e0
Lan <lan.peng234@gmail.com>
2023-08-24 14:20:28 -0700

    Add /songs/ endpoint to hold mp3 on website

--- spotifyBackend/spotify_backend/src/main/kotlin/com/laioffer/Application.kt
+++ spotifyBackend/spotify_backend/src/main/kotlin/com/laioffer/Application.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.http.content.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -62,6 +63,13 @@ fun Application.module() {
             val id: String? = call.parameters["id"]
             val playlist: Playlist? =  playlists.firstOrNull { item -> item.id.toString() == id  }
             call.respondNullable(playlist)
+        }
+
+        static("/") {
+            staticBasePackage = "static"
+            static("songs") {
+                resources("songs")
+            }
         }
     }
 }
