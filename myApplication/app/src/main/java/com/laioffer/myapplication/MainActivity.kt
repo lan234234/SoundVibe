01615187a50b9c6ece2caf355f5e97480d700f8b
Lan <lan.peng234@gmail.com>
2023-08-24 19:49:06 -0700

    Update MainActivity for lifecycle learning

--- myApplication/app/src/main/java/com/laioffer/myapplication/MainActivity.kt
+++ myApplication/app/src/main/java/com/laioffer/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.laioffer.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,9 +14,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.laioffer.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val TAG = "lifecycle"
+
+    // create new object
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+
+        Log.d(TAG, "onCreate")
+
+        setContent { // content is UI
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -26,6 +34,40 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        // networking call
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // UI is ready
+
+        Log.d(TAG, "onResume")
+    }
+
+    // --------------------------
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop")
+        // UI destroyed
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
 
