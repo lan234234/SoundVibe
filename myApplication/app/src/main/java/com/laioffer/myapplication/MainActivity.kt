169372f3a74f72cfa640d1a49e219f5cfd558afb
Lan <lan.peng234@gmail.com>
2023-08-25 23:42:48 -0700

    Update MainActivity for learning compose layout

--- myApplication/app/src/main/java/com/laioffer/myapplication/MainActivity.kt
+++ myApplication/app/src/main/java/com/laioffer/myapplication/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.laioffer.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
                 }
             }
         }
@@ -38,44 +49,60 @@ class MainActivity : ComponentActivity() {
         // networking call
     }
 
-    override fun onStart() {
-        super.onStart()
+//    1. learning life cycle-------------------
 
-        Log.d(TAG, "onStart")
+//    override fun onStart() {
+//        super.onStart()
+//
+//        Log.d(TAG, "onStart")
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        // UI is ready
+//
+//        Log.d(TAG, "onResume")
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        Log.d(TAG, "onPause")
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//
+//        Log.d(TAG, "onStop")
+//        // UI destroyed
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        Log.d(TAG, "onDestroy")
+//    }
+}
+
+//    2. learning compose layout-------------------
+
+@Composable
+fun ArtistCardBox() {
+    Column {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
     }
+}
 
-    override fun onResume() {
-        super.onResume()
-        // UI is ready
-
-        Log.d(TAG, "onResume")
-    }
-
-    // --------------------------
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        Log.d(TAG, "onStop")
-        // UI destroyed
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
+@Preview
+@Composable
+fun previewArtistCardBox() {
+    ArtistCardBox()
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier.background(Color.Green)
     )
 }
 
@@ -86,3 +113,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
